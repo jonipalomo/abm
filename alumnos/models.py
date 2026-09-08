87516eaecf0fbb194395bd901754ea701e01edc3
equalitybeta-ux <equalitybeta@gmail.com>
2026-09-08 02:37:42 +0200

    Actualiza modelo Alumno y Curso

--- alumnos/models.py
+++ alumnos/models.py
@@ -1,4 +1,13 @@
+```python
 from django.db import models
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    turno = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.turno}'
 
 
 class Alumno(models.Model):
@@ -6,7 +15,11 @@ class Alumno(models.Model):
     apellido = models.CharField(max_length=100)
     dni = models.CharField(max_length=15, unique=True)
     email = models.EmailField(blank=True)
-    curso = models.CharField(max_length=50)
+    curso = models.ForeignKey(
+        Curso,
+        on_delete=models.PROTECT,
+        related_name='alumnos'
+    )
     fecha_nacimiento = models.DateField(null=True, blank=True)
     activo = models.BooleanField(default=True)
     creado = models.DateTimeField(auto_now_add=True)
